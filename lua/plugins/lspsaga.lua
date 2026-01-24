cf769636ee8fbfcf1a36b3c6b21de48f8d58553c
InNiD <D2862519962@outlook.com>
2026-01-24 16:57:01 +0800

    chore: droped lspsaga and add keymaps in telescope

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,5 +1,6 @@
 return {
     "nvimdev/lspsaga.nvim",
+    enabled = false,
     version = "*",
     event = "LspAttach",
     dependencies = {
@@ -21,14 +22,14 @@ return {
         },
     },
     keys = {
-        { "<leader>rn", ":Lspsaga rename ++project<CR>", silent = true },
-        { "<leader>ca", ":Lspsaga code_action<CR>", silent = true },
-        { "<leader>gd", ":Lspsaga goto_definition<CR>", silent = true },
-        { "<leader>hd", ":Lspsaga hover_doc<CR>", silent = true },
-        { "<leader>li", ":Lspsaga finder<CR>", silent = true },
-        { "<leader>nd", ":Lspsaga diagnostic_jump_next<CR>", silent = true },
-        { "<leader>pd", ":Lspsaga diagnostic_jump_prev<CR>", silent = true },
-        { "<leader>sd", ":Lspsaga show_line_diagnostics<CR>", silent = true },
-        { "<leader>ol", ":Lspsaga outline<CR>", silent = true},
+        -- { "grn", ":Lspsaga rename ++project<CR>", silent = true },
+        -- { "gra", ":Lspsaga code_action<CR>", silent = true },
+        -- { "gd", ":Lspsaga goto_definition<CR>", silent = true },
+        -- { "K", ":Lspsaga hover_doc<CR>", silent = true },
+        -- { "grr", ":Lspsaga finder<CR>", silent = true },
+        -- { "<leader>nd", ":Lspsaga diagnostic_jump_next<CR>", silent = true },
+        -- { "<leader>pd", ":Lspsaga diagnostic_jump_prev<CR>", silent = true },
+        -- { "<leader>sd", ":Lspsaga show_line_diagnostics<CR>", silent = true },
+        -- { "gO", ":Lspsaga outline<CR>", silent = true },
     },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -45,6 +45,15 @@ return {
         { "<leader>fg", "<Cmd>Telescope live_grep<CR>", desc = "live grep", silent = true },
         { "<leader>fc", "<Cmd>Telescope git_commits<CR>", desc = "git commits", silent = true },
         { "<leader>fo", "<Cmd>Telescope oldfiles<CR>", desc = "find oldfiles", silent = true },
-        { "<leader>fb", "<Cmd>Telescope buffers<CR>", desc = "Telescope buffers", silent = true }
+        { "<leader>fb", "<Cmd>Telescope buffers<CR>", desc = "Telescope buffers", silent = true },
+
+        { "gd", "<Cmd>Telescope lsp_definitions<CR>", desc = "go to definition", silent = true },
+        { "gO", "<Cmd>Telescope lsp_document_symbols<CR>", desc = "show document symbols", silent = true },
+        { "gri", "<Cmd>Telescope lsp_implementations<CR>", desc = "go to implementations", silent = true },
+        { "grr", "<Cmd>Telescope lsp_references<CR>", desc = "go to regerences", silent = true },
+        { "grt", "<Cmd>Telescope lsp_type_definitions<CR>", desc = "go to type_defineittions", silent = true },
+        { "<leader>ld", "<Cmd>Telescope diagnostics<CR>", desc = "list diagnostics", silent = true },
+        { "<leader>li", "<Cmd>Telescope lsp_incoming_calls<CR>", desc = "list where use", silent = true },
+        { "<leader>lo", "<Cmd>Telescope lsp_outgoing_calls<CR>", desc = "list use what", silent = true },
     },
 }
