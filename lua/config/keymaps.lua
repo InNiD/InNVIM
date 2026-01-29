e50ce05f75cb7f03d972fe1edc4e0a25d65d2893
InNiD <D2862519962@outlook.com>
2026-01-29 10:28:10 +0800

    chore: change a keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -32,7 +32,7 @@ vim.keymap.set({ "n", "i", "v", "t"}, "<A-Right>", "<Cmd>vertical resize +2<CR>"
 vim.keymap.set("n", "x", '"_x', opts)
 vim.keymap.set("n", "s", '"_s', opts)
 
-vim.keymap.set("n", "<leader>m", ":<c-u><c-r><c-r>='let @'. v:register .' = '. string(getreg(v:register))<cr><c-f><left>", opts)
+vim.keymap.set("n", "<leader>cr", ":<c-u><c-r><c-r>='let @'. v:register .' = '. string(getreg(v:register))<cr><c-f><left>", opts)
 
 -----------------
 -- INSERT mode --
