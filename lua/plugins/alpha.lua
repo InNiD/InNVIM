e948d665b8f3f1606135a89aa36c7dfcaf6810fe
InNiD <D2862519962@outlook.com>
2026-01-29 23:51:00 +0800

    feat: enable alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,22 +1,22 @@
 return {
     "goolord/alpha-nvim",
-    enabled = false,
+    -- enabled = false,
     version = "*",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         -- startify, dashboard, theta
-        local startify = require("alpha.themes.startify")
-        startify.file_icons.provider = "devicons"
-        startify.section.header.val = {
-             [[                               __                ]],
-             [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-             [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-             [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-             [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-             [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+        local theme = require("alpha.themes.startify")
+        -- theme.file_icons.provider = "devicons"
+        theme.section.header.val = {
+                [[██╗███╗   ██╗███╗   ██╗██╗   ██╗██╗███╗   ███╗]],
+                [[██║████╗  ██║████╗  ██║██║   ██║██║████╗ ████║]],
+                [[██║██╔██╗ ██║██╔██╗ ██║██║   ██║██║██╔████╔██║]],
+                [[██║██║╚██╗██║██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
+                [[██║██║ ╚████║██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║]],
+                [[╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝]],
         }
-        require("alpha").setup({
-            startify.config
-        })
+        require("alpha").setup(
+            theme.config
+        )
     end,
 }
