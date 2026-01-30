765b6c77332966c50f0c9da1097e05a8649216ce
InNiD <D2862519962@outlook.com>
2026-01-30 11:00:36 +0800

    chore: change some settings of alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -2,21 +2,30 @@ return {
     "goolord/alpha-nvim",
     -- enabled = false,
     version = "*",
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        -- "nvim-lua/plenary.nvim",
+    },
     config = function()
         -- startify, dashboard, theta
-        local theme = require("alpha.themes.startify")
+        local theme = require("alpha.themes.dashboard")
         -- theme.file_icons.provider = "devicons"
         theme.section.header.val = {
-                [[██╗███╗   ██╗███╗   ██╗██╗   ██╗██╗███╗   ███╗]],
-                [[██║████╗  ██║████╗  ██║██║   ██║██║████╗ ████║]],
-                [[██║██╔██╗ ██║██╔██╗ ██║██║   ██║██║██╔████╔██║]],
-                [[██║██║╚██╗██║██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
-                [[██║██║ ╚████║██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║]],
-                [[╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+            "██╗███╗   ██╗███╗   ██╗██╗   ██╗██╗███╗   ███╗",
+            "██║████╗  ██║████╗  ██║██║   ██║██║████╗ ████║",
+            "██║██╔██╗ ██║██╔██╗ ██║██║   ██║██║██╔████╔██║",
+            "██║██║╚██╗██║██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+            "██║██║ ╚████║██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║",
+            "╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝",
         }
-        require("alpha").setup(
-            theme.config
-        )
+        theme.section.buttons.val = {
+            theme.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            theme.button("y", "  File explorer", ":Yazi<CR>"),
+            theme.button("f", "  Find file", ":Telescope find_files<CR>"),
+            theme.button("o", "  Find oldfile", ":Telescope oldfiles<CR>"),
+            theme.button("g", "󰊄  Find word", "<Cmd>Telescope live_grep<CR>"),
+            theme.button("q", "󰅚  Quit InNVIM" , ":qa<CR>"),
+        }
+        require("alpha").setup(theme.config)
     end,
 }
